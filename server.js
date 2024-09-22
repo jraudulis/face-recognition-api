f0255c4146dbd6b4951601e1b916c464ceecb87c
Juris Raudulis <juris.raudulis@outlook.com>
2024-09-22 16:29:56 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'postgres',
+    password: 'bober',
+    database: 'face-recognition'
+  }
+});
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) =>{
+ res.send('succes');
+});
+
+app.post('/signin', (req, res) =>{
+  db.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+  .then(data => {
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+    if(isValid) {
+      return db.select('*').from('users')
+      .where('email', '=', req.body.email)
+      .then(user => {
+        res.json(user[0])
+      })
+      .catch(err => res.status(400).json('user not found'))
+    } else {
+      res.status(400).json('wrong login or pass')
+    }
+  })
+  .catch(err => res.status(400).json('something went wrong'))
+});
+
+app.post('/register', (req, res) =>{
+ const { email, name, password } = req.body;
+ const hash = bcrypt.hashSync(password, saltRounds);
+ db.transaction(trx => {
+   trx.insert({
+    hash: hash,
+    email: email
+   })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+      .returning('*')
+      .insert({
+        email: loginEmail[0].email,
+        name: name,
+        joined: new Date()
+      })
+      .then(user =>{
+       res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('unable to register')) 
+});
+
+app.get('/profile/:id', (req, res) =>{
+
+ const { id } = req.params;
+ db.select('*').from('users').where({id})
+ .then(user => {
+  if(user.length) {
+    res.json(user[0]);
+  } else {
+    res.status(400).json('user not found');
+  }
+ })
+ .catch(err => res.status(200).json('error of some sort'));
+});
+
+app.put('/image', (req, res) =>{
+ const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries =>{
+    res.json(entries[0].entries);
+})
+  .catch(err => res.status(400).json('unable to respond'));
+});
+
+app.listen(3001, () =>{
+ console.log('server is running');
+});
+
+
+
